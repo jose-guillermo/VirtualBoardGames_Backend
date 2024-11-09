c50d8c7668191dde0360f7c004878bdb3cbd05b4
DESKTOP-IT93E60\joseg <joseguille.jbc@gmail.com>
2024-11-09 18:23:30 +0100

    Primer commit

---
+++ conn.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php'; 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+$host= getenv("DB_HOST")
+$user= getenv("DB_USER")
+$host= getenv("DB_HOST")
+$host= getenv("DB_HOST")
+$host= getenv("DB_HOST")
+
+$conn = pg_connect("");
+?>
